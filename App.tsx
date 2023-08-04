ee91da2b4b47414cd50e6f22593aed03fc9b246d
John William <johnwilliams1756@gmail.com>
2023-08-04 04:01:05 +0300

    auth

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import List from './app/screens/List';
-import Details from './app/screens/Details';
 import Login from './app/Login';
 
 const Stack = createNativeStackNavigator();
@@ -12,9 +8,9 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName='Login'>
+    <Stack.Navigator initialRouteName='Login' >
+      <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
       <Stack.Screen name="Todos" component={List} />
-      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   </NavigationContainer>
   );
--- app/Login.tsx
+++ app/Login.tsx
@@ -1,32 +1,80 @@
-import { View, TextInput, Button, StyleSheet } from 'react-native'
-import React,{useState} from 'react'
+import { View, Text, TextInput, Button, StyleSheet, KeyboardAvoidingView, TouchableOpacity } from 'react-native'
+import React,{useEffect, useState} from 'react'
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigation } from '@react-navigation/native';
+
 
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (user) {
+                // @ts-ignore
+                navigation.navigate('Todos');
+            }
+        })
+        return unsubscribe
+    },[])
+
     const auth = getAuth();
 
-    const signUp = async() => {
-        const after = await createUserWithEmailAndPassword(auth, email, password)
+    const handleSignUp = async() => {
+        try {
+           const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+           const user = userCredential.user;
+        //    console.log('Registered With', user.email);
+        } catch (error) {
+            if (error instanceof Error) {
+                alert(error.message);
+            } else {
+                alert('An unexpected error occured.')
+            }
+        }
 
     }
 
-    const signIn = async() => {
-        const user = await signInWithEmailAndPassword(auth, email, password)
+    const handleLogin = async() => {
+        try {
+           const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+           const user = userCredential.user;
+        //    console.log('logged in with', user.email);
+        } catch (error) {
+            if (error instanceof Error) {
+                alert(error.message);
+            } else {
+                alert('An unexpected error occured.')
+            }
+        }
+
     }
+
+  
 
   return (
-    <View style={styles.container}>
-       <TextInput style={styles.input} onChangeText={(text: string)=> setEmail(text)} value={email} placeholder='Email'/>
-       <TextInput style={styles.input} onChangeText={(text: string)=> setPassword(text)} value={password} placeholder='Password' textContentType='password'/>
+    <KeyboardAvoidingView style={styles.container} >
+        <View style={styles.inputContainer}>
+        <TextInput style={styles.input} onChangeText={(text: string)=> setEmail(text)} value={email} placeholder='Email'/>
+        <TextInput style={styles.input} onChangeText={(text: string)=> setPassword(text)} value={password} placeholder='Password' secureTextEntry/>
+        </View>
+        
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={handleLogin} style={styles.button}>
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
 
-       <Button onPress={signUp} title='create account'/>
-       <Button onPress={signIn} title='Sign In'/>
-
-    </View>
+            <TouchableOpacity onPress={handleSignUp} style={[styles.button, styles.buttonOutline]}>
+                <Text style={styles.buttonOutlineText}>Register</Text>
+            </TouchableOpacity>
+        </View>
+    </KeyboardAvoidingView>
+    
   )
 }
 
@@ -34,15 +82,46 @@ export default Login;
 
 const styles = StyleSheet.create({
     container: {
-        marginHorizontal: 20,
-        paddingVertical: 20,
+        flex: 1, 
+        justifyContent: 'center',
+        alignItems:'center',
+    },
+    inputContainer: {
+        width: '80%',
     },
     input: {
         backgroundColor:'#fff',
-        borderRadius: 5,
-        borderWidth: 1,
-        height: 50,
-        padding: 10,
-        marginVertical: 5
+        marginTop: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+    },
+    buttonContainer:{
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    button:{
+        backgroundColor: '#0782F9',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    buttonText:{
+        color: '#fff',
+        fontWeight: '700',
+        fontSize: 10,
+    },
+    buttonOutline:{
+        backgroundColor: '#fff',
+        marginTop: 5,
+        borderColor: '#0782F9',
+        borderWidth: 2,
+    },
+    buttonOutlineText:{
+        color: '#0782F9',
+        fontWeight:'700',
+        fontSize: 16,
     }
 });
